2dc6075f5498ace91152d9c4d0177c577a135d2d
Maragues <miguelaragues@gmail.com>
2017-01-17 13:04:30 +0100

    Add nullable annotation to ISynchronizable so that we can create local ojects

--- app/src/main/java/com/maragues/menu_planner/model/ISynchronizable.java
+++ app/src/main/java/com/maragues/menu_planner/model/ISynchronizable.java
@@ -1,11 +1,13 @@
 package com.maragues.menu_planner.model;
 
 import android.os.Parcelable;
+import android.support.annotation.Nullable;
 
 /**
  * Created by miguelaragues on 22/12/16.
  */
 
 public interface ISynchronizable extends Parcelable {
+  @Nullable
   String id();
 }
